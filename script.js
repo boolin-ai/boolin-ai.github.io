b7a2ce82c17b1fa38770819eb9d7b607daaf11d9
Makoto <boolin@MakotonoMac-mini.local>
2024-04-09 20:36:59 +0900

    abe

---
+++ script.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const liffId = "2000050276-Kjj7lW0L"; // ここにLIFF IDを設定
+    initializeLiff(liffId);
+
+    // formStart要素を取得
+    const formStart = document.getElementById('formStart');
+
+    // formStart要素が存在するか確認し、イベントリスナーを追加
+    if (formStart) {
+        document.getElementById("formStart").onclick = function() {
+            // ページ遷移先のURL
+            const targetPath = "/form";
+            
+            // LIFF内でのページ遷移
+            liff.navigate(targetPath);
+        };
+    }
+});
+
+function initializeLiff(liffId) {
+    liff.init({
+        liffId: liffId
+    }).then(() => {
+        if (liff.isLoggedIn()) {
+            initializeApp();
+            // 初期化成功後、ボタンにクリックイベントリスナーを追加
+            document.getElementById('formStart').addEventListener('click', () => {
+                // ここで指定したパスに遷移
+                liff.navigate('/form.html');
+            });
+        } else {
+            liff.login(); // ユーザーがログインしていない場合はログインを促す
+        }
+    }).catch((err) => {
+        console.error('LIFF Initialization failed', err);
+    });
+}
+
+function initializeApp() {
+    liff.getProfile().then(profile => {
+        const userId = profile.userId;
+        const displayName = profile.displayName;
+    }).catch(err => console.error(err));
+}
+
+// LIFF AppのエンドポイントURL
+const liffEndpointUrl = "https://liff.line.me/2000050276-Kjj7lW0L/form.html";
+
+// パーマリンクを作成するための関数
+function createPermanentLink(pageUrl) {
+liff.permanentLink
+    .createUrlBy(pageUrl)
+    .then((permanentLink) => {
+    console.log(permanentLink);
+    })
+    .catch((error) => {
+    console.log(error);
+    });
+}
